8bf263e95916413c4c1512ac9a78faed138c179d
5iveself <78660200+5iveself@users.noreply.github.com>
2023-09-02 23:34:00 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -43,7 +43,7 @@ local Window = OrionLib:MakeWindow({Name = "Punch Simulator", HidePremium = fals
 
 local Tab = Window:MakeTab({
 	Name = "Autofarm",
-	Icon = "rbxassetid://4483345998",
+	Icon = "rbxassetid://9913141725",
 	PremiumOnly = false
 })
 
